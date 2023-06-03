dfa1ea5500d9f1971e95c8fafcb365f492a5718e
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2023-06-02 19:29:17 -0700

    updated NASA api endpoint

--- src/App.js
+++ src/App.js
@@ -4,29 +4,29 @@ import Loader from './components/Loader';
 import Header from './components/Header';
 
 function App() {
-
-  const [eventData, setEventData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
       setLoading(true);
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const res = await fetch(
+        'https://eonet.gsfc.nasa.gov/api/v3/events?category=wildfires'
+      );
 
-      const { events } = await res.json()
+      const { events } = await res.json();
 
       setEventData(events);
       setLoading(false);
-    }
+    };
 
     fetchEvents();
-
-  }, [])
+  }, []);
 
   return (
     <div>
       <Header />
-      { !loading ? <Map eventData={eventData} /> : <Loader />}
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
